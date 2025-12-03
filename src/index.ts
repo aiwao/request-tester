b2d4eddf0ddb2507864f35e6223f8cf630a6e2ba
aiwao <233898056+aiwao@users.noreply.github.com>
2025-12-03 15:56:39 +0900

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.all("/", async (c) => {
+  let res: string[] = []
+  res.push(`URL: ${c.req.raw.url}`)
+  res.push(`Method: ${c.req.raw.method}`)
+  let header: string[] = []
+  if (c.req.raw.headers !== null) {
+    c.req.raw.headers.forEach((value, key) => {
+      header.push(`${key}: ${value}`)
+    })
+  }
+  res.push(`Header: {\n${header.join("\n")}\n}`)
+  let body = null
+  if (c.req.raw.body !== null) {
+    body = await c.req.text()
+  }
+  res.push(`Body: ${body}`)
+  return c.text(res.join("\n"))
+})
+
+export default app
